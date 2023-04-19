a9dd9ef2fd96562bd0b51334c2e648a0fd695fdc
Ellie <111202585+flowersin@users.noreply.github.com>
2023-04-19 09:47:59 -0600

    able to display grid of symbols now!

--- EarthSim.py
+++ EarthSim.py
@@ -1,5 +1,6 @@
 # A life simulator thing
 import random
+import pygame as pg
 
 GRID_ROWS = 3
 GRID_COLUMNS = 3
@@ -20,7 +21,12 @@ def make_grid():
             grid[row].append(random_tile())
     return grid
 
+def print_grid_symbols(grid):
+    for row in range(GRID_ROWS):
+        for column in range(GRID_COLUMNS):
+            print(grid[row][column]['symbol'], end='')
+        print('')
+
 if __name__ == '__main__': 
     grid = make_grid()
-    print(grid)
-
+    print_grid_symbols(grid)
